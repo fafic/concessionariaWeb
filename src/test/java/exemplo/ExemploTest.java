05c9ff9dba1618c53eb59e9a72662ffbbf92f31b
Luciano <lucciano01@hotmail.com>
2020-06-03 11:33:15 -0300

    alterando porta procfile

---
+++ src/test/java/exemplo/ExemploTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplo;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+/**
+ *
+ * @author Luciano
+ */
+public class ExemploTest {
+    
+    
+   // @Test
+    public void deveriaCompararDoisValores(){
+        int a = 5;
+        int b = 5;
+        assertEquals(b, a);
+    }
+    
+}
